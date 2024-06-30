2bca54d443431fe42c2c393a6401aa05172d177c
BYUNGMIN CHOI <nekarakube@gmail.com>
2024-06-30 21:16:23 +0900

    조건부로 dotenv 사용하도록 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,13 @@
-require("dotenv").config({
-    path: `.env`,
-});
+const fs = require('fs');
+const path = require('path');
+
+// .env 파일이 존재하는지 확인하고, 존재하면 dotenv 로드
+const envPath = path.resolve(__dirname, '.env');
+if (fs.existsSync(envPath)) {
+    require('dotenv').config({
+        path: envPath,
+    });
+}
 
 module.exports = {
     pathPrefix: "/blog",
